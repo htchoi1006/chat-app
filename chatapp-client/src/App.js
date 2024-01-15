74debc183a8a1180d2d4fae148eb2604e935ded6
htchoi1006 <htchoi1006@kookmin.ac.kr>
2024-01-16 00:05:35 +0900

    채팅 페이지 구현

---
+++ chatapp-client/src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import socket from "./server";
+import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
+
+function App() {
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([])
+  console.log("message list", messageList);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessageList((prevState) => prevState.concat(message));
+    })
+    askUserName();
+  }, []);
+
+  const askUserName = () => {
+    const userName = prompt("이름을 입력하세요.");
+    console.log("uuu", userName);
+
+    socket.emit("login", userName, (res) => {
+      console.log("res", res);
+      if(res?.ok){
+        setUser(res.data);
+        console.log("user info", userName);
+      };
+    });
+  }
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    socket.emit("sendMessage", message, (res) => {
+      console.log("sendMessage res", res);
+    });
+  };
+
+
+  return (
+    <div>
+      <div className="App">
+        <MessageContainer messageList={messageList} user={user} />
+        <InputField message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ chatapp-client/src/components/InputField/InputField.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Input } from "@mui/base/Input";
+import { Button } from "@mui/base/Button";
+import './InputField.css'
+const InputField = ({message,setMessage,sendMessage}) => {
+
+  return (
+    <div className="input-area">
+          <div className="plus-button">+</div>
+          <form onSubmit={sendMessage} className="input-container">
+            <Input
+              placeholder="메시지를 입력하세요 ..."
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              multiline={false}
+              rows={1}
+            />
+
+            <Button
+              disabled={message === ""}
+              type="submit"
+              className="send-button"
+            >
+              전송
+            </Button>
+          </form>
+        </div>
+  )
+}
+
+export default InputField
